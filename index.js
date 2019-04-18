4e66f9994cf46f9eb620f81c82bd0fc6cd7d6922
Flavio Tulino <ftulino@bookingbug.com>
2019-04-18 10:28:02 +0100

    Added async version of methods

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-const { execSync } = require('child_process');
+const {execSync, exec} = require('child_process');
 
 class npm {
   constructor() {
     this.options = {
       stdio: 'inherit'
-    }
+    };
     this.args = process.argv.slice(2);
   }
+
   cwd(dir) {
     this.optinos.cwd = dir;
     return this;
   }
+
   output(value = true) {
     this.options.stdio = value ? 'inherit' : 'ignore';
     return this;
   }
-  arguments(args = {}) { 
+
+  arguments(args = {}) {
     if (args === false) {
       this.args = [];
       return this;
     }
-    
+
     const string = [];
-    
+
     Object.keys(args)
-      // in case of yargs
-      .filter(key => key !== '$0' && key !== '_')
-      .forEach(key => {
-        if(Array.isArray(args[key])) {
-          args[key].forEach(value => {
-            string.push(`--${key} ${value}`);
-          })
-        } else {
-          string.push(`--${key} ${args[key]}`);
-        }
-      });
-    
+    // in case of yargs
+        .filter(key => key !== '$0' && key !== '_')
+        .forEach(key => {
+          if (Array.isArray(args[key])) {
+            args[key].forEach(value => {
+              string.push(`--${key} ${value}`);
+            })
+          } else {
+            string.push(`--${key} ${args[key]}`);
+          }
+        });
+
     this.args = string;
     return this;
   }
+
   /**
-  * @executes a npm install, of only one dependency if specified
-  */
+   * @executes a npm install, of only one dependency if specified
+   */
   install(module = null) {
     try {
       return execSync(`npm install ${module}`, this.options);
@@ -49,6 +53,22 @@ class npm {
       return null;
     }
   }
+
+  installAsync(module = null) {
+    return new Promise((resolve, reject) => {
+      try {
+        exec(`npm install ${module}`, this.options, (error, output) => {
+          if (error) {
+            throw error;
+          }
+          return resolve(output);
+        });
+      } catch (e) {
+        return reject(null);
+      }
+    });
+  }
+
   link(module = null) {
     try {
       return execSync(`npm link ${module}`, this.options);
@@ -56,6 +76,22 @@ class npm {
       return null;
     }
   }
+
+  linkAsync(module = null) {
+    return new Promise((resolve, reject) => {
+      try {
+        exec(`npm link ${module}`, this.options, (error, output) => {
+          if (error) {
+            throw error;
+          }
+          return resolve(output);
+        })
+      } catch (e) {
+        reject(null);
+      }
+    });
+  }
+
   run(script) {
     const args = this.args ? `-- ${this.args.join(' ')}` : '';
     try {
@@ -63,6 +99,23 @@ class npm {
     } catch (e) {
       return null;
     }
+  }
+
+  runAsync(script) {
+    const args = this.args ? `-- ${this.args.join(' ')}` : '';
+    return new Promise((resolve, reject) => {
+      try {
+        exec(`npm run ${script} ${args}`, this.options, (error, output) => {
+          if (error) {
+            throw error;
+          }
+
+          return resolve(output)
+        });
+      } catch (e) {
+        return reject(null);
+      }
+    });
   }
 }
 
